0776b642d4c5cb0dd14ef7001c41ac7568be28c2
Sergey Ivanov <koldyn85@yandex.ru>
2023-09-24 15:42:45 +0300

    create entityes

--- server/src/category/entities/category.entity.ts
+++ server/src/category/entities/category.entity.ts
@@ -1,1 +1,21 @@
-export class Category {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn({ name: 'category_id' })
+  id: number;
+
+  @Column()
+  title: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @CreateDateColumn()
+  updetedAt: Date;
+}
--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,4 +1,30 @@
+import { Category } from 'src/category/entities/category.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
   email: string;
+
+  @Column()
   password: string;
+
+  @OneToMany()
+  categories: Category[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updetedAt: Date;
 }
