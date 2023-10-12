e84edba3ad28310d776420f0f7670e517993ccb4
Sergey Ivanov <koldyn85@yandex.ru>
2023-10-12 20:50:21 +0300

    create auth form

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,7 +1,34 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 
 const Auth: FC = () => {
-  return <div>Auth</div>
+  const [isLogin, setIsLogin] = useState<boolean>(false)
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  return <div className="mt-40 flex flex-col justify-center items-center bg-slate-900 text-white">
+    <h1 className="text-center text-xl mb-10">
+      {isLogin ? 'Login' : 'Registration'}
+    </h1>
+
+    <form className="flex w-1/2 flex-col mx-auto gap-5">
+      <input type="text" className="input" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
+      <input type="password" className="input" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+
+      <button className="btn btn-green mx-auto">Submit</button>
+    </form>
+
+    <div className="flex justify-center mt-5">
+      {isLogin ? (
+          <button onClick={() => setIsLogin(!isLogin)} className="text-slate-300 hover:text-white">
+            You don't have an account?
+          </button>
+          ) : (
+            <button onClick={() => setIsLogin(!isLogin)} className="text-slate-300 hover:text-white">
+            Alredy have an account?
+          </button>
+          )
+        }
+    </div>
+  </div>
 }
 
 export default Auth
