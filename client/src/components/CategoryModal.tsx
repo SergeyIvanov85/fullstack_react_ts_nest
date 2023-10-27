b600fa16cbf24b222c4648426ffdac0b1ce99ccd
SergeyIvanov85 <koldyn85@yandex.ru>
2023-10-27 10:34:14 +0300

    fix patch

--- client/src/components/CategoryModal.tsx
+++ client/src/components/CategoryModal.tsx
@@ -2,7 +2,7 @@ import { FC } from "react"
 import { Form } from "react-router-dom"
 
 interface ICategoryModal {
-  type: 'post' | 'path'
+  type: 'post' | 'patch'
   id?: number
   setVisibleModal: (visible: boolean) => void
 }
@@ -12,18 +12,18 @@ export const CategoryModal: FC<ICategoryModal> = ({ type, id, setVisibleModal })
   return (
     <div className="fixed top-0 left-0 bottom-0 right-0 w-full h-full bg-black/50 flex justify-center items-center">
       <Form
-      action="/categories"
-      method={type}
-      onSubmit={() => setVisibleModal(false)}
-      className="grid gap-2 w-[300px] p-5 rouded-md bg-slate-900">
+        action="/categories"
+        method={type}
+        onSubmit={() => setVisibleModal(false)}
+        className="grid gap-2 w-[300px] p-5 rouded-md bg-slate-900">
         <label htmlFor="title">
           <small>Category title</small>
-          <input className="input w-full" type="text" name="title" placeholder="Title..."/>
+          <input className="input w-full" type="text" name="title" placeholder="Title..." />
         </label>
 
         <div className="flex items-center gap-2">
           <button className="btn btn-green" type="submit">
-            { type === 'path' ? 'Save': 'Create'}
+            {type === 'patch' ? 'Save' : 'Create'}
           </button>
           <button onClick={() => setVisibleModal(false)} className="btn btn-red">Close</button>
         </div>
